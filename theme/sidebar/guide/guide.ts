db8f9ea624b4f5a294728042351903579a034b00
codingoer <codingoer@163.com>
2022-05-22 02:02:23 +0800

    add guide sidebar

---
+++ theme/sidebar/guide/guide.ts
@@ -0,0 +1,20 @@
+module.exports = [
+    {
+        text: 'Getting Started',
+        collapsable: true,
+        children: [
+            { text:'Introduction', link:'/guide/get-started/intro' },
+            { text:'Manual Installation', link:'/guide/get-started/install'},
+            { text:'Page Routing', link:'/guide/get-started/page-routing'},
+            { text:'Assets', link:'/guide/get-started/assets'}
+        ]
+    },
+    {
+        text: 'Vurepress Reference',
+        collapsable: true,
+        children: [
+            { text:'VuePress CLI', link:'/guide/vuepress/cli' },
+            { text:'VuePress Config', link:'/guide/vuepress/config' }
+        ]
+    }
+]
