6a77058fb9308871f2c4ef130f65f1b865ec3837
codingoer <codingoer@163.com>
2022-05-23 00:00:20 +0800

    update sidebar

---
+++ theme/sidebar/coding/tools.ts
@@ -0,0 +1,10 @@
+module.exports = [
+    {
+        text: 'Intellij IDEA',
+        collapsable: true,
+        headerDepth: 2,
+        children: [
+            { text:'Intellij IDEA Plugins', link:'/tools/intellij-idea/plugins' }
+        ]
+    }
+]
--- theme/sidebar/sidebar_dev.ts
+++ theme/sidebar/sidebar_dev.ts
@@ -1,5 +1,11 @@
 import {HopeThemeSidebarConfig } from "vuepress-theme-hope";
 
 export const sidebarConf : HopeThemeSidebarConfig = {
+    "/": [
+        "",
+    ],
+    "/guide/": require('./guide/guide'),
     "/java/programing/": require('./java/programing'),
+    //"/java/programing/": "structure",
+    "/tools": require('./coding/tools')
 }
