eb84a43d2105cd74cf45b1a9a0de633fa27153af
codingoer <codingoer@163.com>
2022-05-23 23:17:10 +0800

    update tools idea sidebar

--- theme/sidebar/coding/tools.ts
+++ theme/sidebar/coding/tools.ts
@@ -4,7 +4,12 @@ module.exports = [
         collapsable: true,
         headerDepth: 2,
         children: [
-            { text:'Intellij IDEA Plugins', link:'/tools/intellij-idea/plugins' }
+            { text:'System Settings', link:'/tools/intellij-idea/system-settings' },
+            { text:'Code Style', link:'/tools/intellij-idea/code-style' },
+            { text:'File and Code Templates', link:'/tools/intellij-idea/file-code-templates' },
+            { text:'Live Templates', link:'/tools/intellij-idea/live-templates' },
+            { text:'Plugins', link:'/tools/intellij-idea/plugins' },
+            { text:'Complier', link:'/tools/intellij-idea/complier' }
         ]
     }
 ]
